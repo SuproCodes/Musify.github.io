2c0f081f45a56c6a766ddb52a7857fe497de59b0
SuproCodes <dsupratik1@gmail.com>
2021-10-20 17:44:02 +0530

    Music Player website-only for the music lovers

---
+++ js/music-list.js
@@ -0,0 +1,123 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Raataan Lambiyan",
+    artist: "Tanishk Bagchi x Jubin Nautiyal x Asees Kaur",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "STAY",
+    artist: "The Kid LAROI x Justin Biebar",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Main Dhoondne ko Zamaane Mein",
+    artist: "Arijit Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Shiddat Title Track",
+    artist: "Manan Bhardwaj",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Woman",
+    artist: "Doja cat",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Tum Ho",
+    artist: "Mohit Chauhan x Suzanne D'Mello",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tujhe Kitna Chahne Lage Hum",
+    artist: "Arijit Singh",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Ranjha",
+    artist: "Jasleen Royal x B Praak",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "SugarCrash!",
+    artist: "ElyOtto",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Dil Ko Karaar Aaya",
+    artist: "Yaseer Desai x Neha Kakkar x Rajat Nagpal",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Stereo Hearts",
+    artist: "Gym Class Heroes x Adam Levine",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Into Your Arms(ft. Ava Max)",
+    artist: "Witt Lowry x Ava Max",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Closer",
+    artist: "The Chainsmokers x Hasley",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Woh Din Bhi Kya Din Thhe",
+    artist: "Pritam x Arijit Singh",
+    img: "music-18",
+    src: "music-18"
+  },
+];
+
